03b45a6e5850bbc0f36bc1460534cd669d1f27d0
recursingfeynman <chasedflea089@icloud.com>
2023-02-16 03:08:24 +0300

    Empty commit

--- papyrus.py
+++ papyrus.py
@@ -54,7 +54,7 @@ def get_data(vocab, activity_threshold = 6.25):
     print("Saved as data/raw/data.csv")
 
 # class_names = {'l3': 'SLC superfamily of solute carriers'}
-def get_specific_class(class_names, act_threshold, inact_threshold, version = "latest", drop_duplicates = False):
+def get_specific_class(class_names, act_threshold, inact_threshold, version = "latest", drop_duplicates = False, multiclass = False):
 
     download_papyrus(version=version, structures=False, descriptors = None)
 
@@ -80,9 +80,13 @@ def get_specific_class(class_names, act_threshold, inact_threshold, version = "l
             return 0
         else:
             return np.nan
-
     pdata['activity'] = pdata['pchembl_value_Mean'].apply(lambda x: encode(x, act_threshold, inact_threshold))
     pdata = pdata.dropna(subset = ['activity'])
+    if multiclass:
+        le = LabelEncoder()
+        pdata['activity'] = pdata['accession'] + "-" + pdata['activity'].astype(str)
+        pdata['activity'] = le.fit_transform(pdata['activity'].values)
+
     pdata['activity'] = pdata['activity'].astype(int)
     pdata = pdata.rename(columns = {"SMILES" : 'smiles', "pchembl_value_Mean" : "affinity", "accession" : "protein"})[['smiles', 'sequence', 'activity', 'affinity', 'protein']].reset_index(drop = True)
     print("Molecules: {} \tFeatures ({}): {}".format(*pdata.shape, list(pdata.columns)))
